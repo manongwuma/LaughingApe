91b48eb3e4bbebf8a2c7b85eef32ff5c096f19b7
manongwuma <3292786529@qq.com>
2019-06-17 17:10:27 +0800

    home page dev

--- App.vue
+++ App.vue
@@ -13,5 +13,9 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+	@import url("//at.alicdn.com/t/font_1238779_co2ic3p9di5.css");
+	/*状态栏设置 */
+	.status_bar{
+		height: var(--status-bar-height);
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,22 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+		<!-- 状态栏 -->
+		<view class="status_bar"></view>
+		<!-- 搜索框 -->
+		<view class="search-bar">
+			<view class="school-name">{{ schoolName }}</view>
+			<view class="input-div">
+				<view class="iconfont iconchazhao"></view>
+				{{ placeholder }}
+			</view>
 		</view>
+		<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="300" :circular="true">
+			<swiper-item v-for="(item,index) in swiperList" :key="index">
+				<view class="swiper-item">
+					<image class="imgs" :src="item.src" mode=""></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -11,7 +24,13 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				schoolName: '天津职业大学',
+				placeholder: '资讯、动态、商品',
+				swiperList: [
+					{src:'../../static/banner/banner001.png',url:'http://baidu.com'},
+					{src:'../../static/banner/banner002.png',url:'http://baidu.com'},
+					{src:'../../static/banner/banner003.png',url:'http://baidu.com'},
+				]
 			}
 		},
 		onLoad() {
@@ -23,20 +42,47 @@
 	}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang="scss" scoped>
+	.search-bar {
+		height: 100upx;
+		padding: 0 20upx;
+		font-size: 28upx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+
+		.school-name {
+			width: 200upx;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+			overflow: hidden;
+			color: $uni-base-color;
+		}
+
+		.input-div {
+			height: 60upx;
+			border-radius: 30upx;
+			flex: 1;
+			line-height: 60upx;
+			padding-left: 20upx;
+			color: #999;
+			background-color: #f8f8f8;
+			cursor: pointer;
+
+			.iconchazhao {
+				display: inline-block;
+				margin-right: 15upx;
+			}
+		}
 	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	.swiper {
+		margin: 10upx 30upx;
+		border-radius: 30upx;
+		overflow: hidden;
+		.swiper-item,.imgs{
+			width: 100%;
+			height: 100%;
+		}
 	}
 </style>
